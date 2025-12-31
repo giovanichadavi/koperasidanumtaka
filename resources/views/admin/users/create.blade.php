659cc5380f9cb69c3a420eb9323818f89333fa4e
Giovani Chadavi <giovanichadavi@gmail.com>
2025-12-31 14:48:24 +0800

    a

--- resources/views/admin/users/create.blade.php
+++ resources/views/admin/users/create.blade.php
@@ -1,32 +1,59 @@
 @extends('adminlte::page')
 
+@section('title', 'Tambah Admin')
+
+<link rel="icon" type="image/png" href="{{ asset('favicon.png') }}">
+
+@section('content_header')
+    <div class="container-fluid mt-3"></div>
+@endsection
+
 @section('content')
-<form method="POST" action="{{ route('users.store') }}">
-@csrf
+<div class="row justify-content-center">
+    <div class="col-md-6 col-lg-5">
+        <div class="card">
+            <div class="card-header py-3 px-4">
+                <h5 class="mb-0">Tambah Admin</h5>
+            </div>
 
-<div class="form-group">
-    <label>Nama</label>
-    <input type="text" name="name" class="form-control">
+            <div class="card-body">
+                <form method="POST" action="{{ route('users.store') }}">
+                    @csrf
+
+                    <div class="form-group mb-3">
+                        <label>Nama</label>
+                        <input type="text" name="name"
+                               class="form-control form-control-sm">
+                    </div>
+
+                    <div class="form-group mb-3">
+                        <label>Email</label>
+                        <input type="email" name="email"
+                               class="form-control form-control-sm">
+                    </div>
+
+                    <div class="form-group mb-3">
+                        <label>Password</label>
+                        <input type="password" name="password"
+                               class="form-control form-control-sm">
+                    </div>
+
+                    <div class="form-group mb-4">
+                        <label>Role</label>
+                        <select name="role" class="form-control form-control-sm">
+                            <option value="admin">Admin</option>
+                            <option value="user">User</option>
+                        </select>
+                    </div>
+
+                    <div class="text-end">
+                        <button class="btn btn-primary btn-sm px-4">
+                            Simpan
+                        </button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
 </div>
-
-<div class="form-group">
-    <label>Email</label>
-    <input type="email" name="email" class="form-control">
-</div>
-
-<div class="form-group">
-    <label>Password</label>
-    <input type="password" name="password" class="form-control">
-</div>
-
-<div class="form-group">
-    <label>Role</label>
-    <select name="role" class="form-control">
-        <option value="admin">Admin</option>
-        <option value="user">User</option>
-    </select>
-</div>
-
-<button class="btn btn-primary">Simpan</button>
-</form>
 @endsection
